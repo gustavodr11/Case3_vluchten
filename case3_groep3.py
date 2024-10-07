bd2a63044b3c3702add5a17b32f1e90f28c20c68
Gustavo de Rouville <129081063+gustavodr11@users.noreply.github.com>
2024-10-07 14:57:23 +0200

    Update case3_groep3.py

--- case3_groep3.py
+++ case3_groep3.py
@@ -2,5 +2,3 @@ import streamlit as st
 
 st.set_page_config(page_title='Test', page_icon='✈️')
 
-create_page = st.Page("create.py", title="Create entry", icon=":material/add_circle:")
-delete_page = st.Page("delete.py", title="Delete entry", icon=":material/delete:")
