8e54a6d3877411597e4d7f6d20b22a2e13ce28db
Gustavo de Rouville <129081063+gustavodr11@users.noreply.github.com>
2024-10-07 14:56:46 +0200

    Update case3_groep3.py

--- case3_groep3.py
+++ case3_groep3.py
@@ -2,7 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title='Test', page_icon='✈️')
 
-with st.sidebar:
-    intro_button = st.button('Intro')
-    flights_button = st.button('Vluchten')
-    airports_button = st.button('Airports')
+create_page = st.Page("create.py", title="Create entry", icon=":material/add_circle:")
+delete_page = st.Page("delete.py", title="Delete entry", icon=":material/delete:")
