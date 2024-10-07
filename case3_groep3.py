ed6967dc33a616f8f2e3b43993d063b59e21ab01
Gustavo de Rouville <129081063+gustavodr11@users.noreply.github.com>
2024-10-07 14:26:58 +0200

    Update case3_groep3.py

--- case3_groep3.py
+++ case3_groep3.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title='Test', page_icon='✈️')
 
-with st.sidebar:
-    if st.button('Intro'):
-    if st.button('Vluchten'):
+with st.sidebar: 
+    if st.button('Intro'): 
+        
+    if st.button('Vluchten'): 
+        
     if st.button('Airports'):
