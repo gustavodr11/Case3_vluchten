cb78e255d5794dd33a9f41e6c2f3ab6e4c313955
Gustavo de Rouville <129081063+gustavodr11@users.noreply.github.com>
2024-10-08 10:32:33 +0200

    Update case3_groep3.py

--- case3_groep3.py
+++ case3_groep3.py
@@ -5,5 +5,5 @@ st.set_page_config(page_title='Case 3 Vluchten (groep 3)', page_icon='✈️')
 
 # sidebar
 with st.sidebar: 
-  selected = option_menu(menu_title="Menu", options=["Intro", "Vluchten", "Luchthavens"], icons=["play", "airplane", "bezier"])
+  selected = option_menu(menu_title="Menu", options=["Intro", "Vluchten", "Luchthavens"], icons=["play", "airplane", "bezier"], menu_icon="list")
 
