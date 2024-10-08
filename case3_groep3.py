22c5bf873404a85f7976e46c65fa3389a89de68b
Gustavo de Rouville <129081063+gustavodr11@users.noreply.github.com>
2024-10-08 10:20:02 +0200

    Update case3_groep3.py

--- case3_groep3.py
+++ case3_groep3.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title='Case 3 Vluchten (groep 3)', page_icon='✈️')
+
+# sidebar
+with st.sidebar: 
+  selected = option_menu(menu_title="Menu", options=["Intro", "Vluchten", "Luchthavens"])
 
