45c9b1acfe21c1290efb042f90c8ac43f1000488
Tinus66 <martijn.de.jong4@hva.nl>
2024-10-09 17:02:09 +0200

    Update case3_groep3.py

--- case3_groep3.py
+++ case3_groep3.py
@@ -149,7 +149,7 @@ if selected == "Vluchten":
 # Vluchten pagina
 if selected == 'Luchthavens':
   st.title("Luchthavens") 
-  st.supheader("Top 20 luchthavens") 
+  st.subheader("Top 20 luchthavens") 
   df = (DatasetLuchthaven_murged2.csv)
   # Tellen van de meest voorkomende luchthavens
   luchthaven_frequentie = df['luchthaven'].value_counts().nlargest(20).reset_index()
