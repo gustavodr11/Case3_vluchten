e64d3f25f174a5166cca9f7a3f4fe0a7ddec474b
Gustavo de Rouville <129081063+gustavodr11@users.noreply.github.com>
2024-11-05 08:21:24 +0100

    Update Case3_versie2.py

--- Case3_versie2.py
+++ Case3_versie2.py
@@ -1,1 +1,18 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+import numpy as np
+import folium
+import branca.colormap as cm
+from streamlit_folium import st_folium
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from folium.plugins import HeatMap
 
+
+st.set_page_config(page_title='Case 3 Vluchten (groep 3)', page_icon='✈️', layout='wide', initial_sidebar_state='expanded')
+
+# sidebar
+with st.sidebar: 
+  selected = option_menu(menu_title="Menu", options=["Intro", "Vluchten", "Luchthavens"], icons=["play", "airplane", "bezier"], menu_icon="list")
