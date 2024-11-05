f3444e4c5dcd8bbb1214b020f46bdbf36dcc3887
Gustavo de Rouville <129081063+gustavodr11@users.noreply.github.com>
2024-11-05 08:25:52 +0100

    Update Case3_versie2.py

--- Case3_versie2.py
+++ Case3_versie2.py
@@ -17,3 +17,23 @@ st.set_page_config(page_title='Case 3 Vluchten (groep 3)', page_icon='✈️', l
 with st.sidebar: 
   st.sidebar.header('Dashboard `versie 2`')
   selected = option_menu(menu_title="Menu", options=["Intro", "Vluchten", "Luchthavens"], icons=["play", "airplane", "bezier"], menu_icon="list")
+
+# --------------------------------------------------------------------------
+
+# INTRO pagina
+if selected == 'Intro':
+    st.title("Case 3 Vluchten - Groep 3")
+
+    # Korte uitleg
+    st.write("""
+        (uitleg...)
+    """)
+  
+    # Bronnen
+    st.write("### Gebruikte Bronnen:")
+    st.write("""
+        - [Youtube filmpje](https://www.youtube.com/watch?v=hEPoto5xp3k)
+        - [Streamlit documentatie](https://docs.streamlit.io/)
+    """)
+
+# --------------------------------------------------------------------------
